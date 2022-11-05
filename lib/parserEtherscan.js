naddison36/sol2uml

commit f6fe794c8fb0d5d12fdf416ab1c084fcefd0d170

feat: added support for Avalanche, Crono, Fantom, Moonbeam and Optimistic fix: sol2uml version now handles js and ts execution chore: better handling of errors chore: bumped version to 2.1.4

lib/parserEtherscan.js
@@ -3,13 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.EtherscanParser = void 0;
+exports.EtherscanParser = exports.networks = void 0;
 const axios_1 = __importDefault(require("axios"));
 const parser_1 = require("@solidity-parser/parser");
 const converterAST2Classes_1 = require("./converterAST2Classes");
 const filterClasses_1 = require("./filterClasses");
 const debug = require('debug')('sol2uml');
-const networks = [
+exports.networks = [
     'mainnet',
     'ropsten',
     'kovan',
@@ -17,15 +17,26 @@ const networks = [
     'goerli',
     'sepolia',
     'polygon',
-    'bsc',
+    'testnet.polygon',
     'arbitrum',
+    'testnet.arbitrum',
+    'avalanche',
+    'testnet.avalanche',
+    'bsc',
+    'testnet.bsc',
+    'crono',
+    'fantom',
+    'testnet.fantom',
+    'moonbeam',
+    'optimistic',
+    'kovan-optimistic',
 ];
 class EtherscanParser {
     constructor(apikey = 'ZAD4UI2RCXCQTP38EXS3UY2MPHFU5H9KB1', network = 'mainnet') {
         this.apikey = apikey;
         this.network = network;
-        if (!networks.includes(network)) {
-            throw new Error(`Invalid network "${network}". Must be one of ${networks}`);
+        if (!exports.networks.includes(network)) {
+            throw new Error(`Invalid network "${network}". Must be one of ${exports.networks}`);
         }
         else if (network === 'mainnet') {
             this.url = 'https://api.etherscan.io/api';
@@ -34,12 +45,52 @@ class EtherscanParser {
             this.url = 'https://api.polygonscan.com/api';
             this.apikey = 'AMHGNTV5A7XYGX2M781JB3RC1DZFVRWQEB';
         }
-        else if (network === 'bsc') {
-            this.url = 'https://api.bscscan.com/api';
-            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN';
+        else if (network === 'testnet.polygon') {
+            this.url = 'https://api-testnet.polygonscan.com/api';
+            this.apikey = 'AMHGNTV5A7XYGX2M781JB3RC1DZFVRWQEB';
         }
         else if (network === 'arbitrum') {
             this.url = 'https://api.arbiscan.io/api';
+            this.apikey = 'ZGTK2TAGWMAB6IAC12BMK8YYPNCPIM8VDQ';
+        }
+        else if (network === 'testnet.arbitrum') {
+            this.url = 'https://api-testnet.arbiscan.io/api';
+            this.apikey = 'ZGTK2TAGWMAB6IAC12BMK8YYPNCPIM8VDQ';
+        }
+        else if (network === 'avalanche') {
+            this.url = 'https://api.snowtrace.io/api';
+            this.apikey = 'U5FAN98S5XNH5VI83TI4H35R9I4TDCKEJY';
+        }
+        else if (network === 'testnet.avalanche') {
+            this.url = 'https://api-testnet.snowtrace.io/api';
+            this.apikey = 'U5FAN98S5XNH5VI83TI4H35R9I4TDCKEJY';
+        }
+        else if (network === 'bsc') {
+            this.url = 'https://api.bscscan.com/api';
+        }
+        else if (network === 'testnet.bsc') {
+            this.url = 'https://api-testnet.bscscan.com/api';
+        }
+        else if (network === 'crono') {
+            this.url = 'https://api.cronoscan.com/api';
+            this.apikey = '76A3RG5WHTPMMR66E9SFI2EIDT6MP976W2';
+            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN';
+        }
+        else if (network === 'fantom') {
+            this.url = 'https://api.ftmscan.com/api';
+            this.apikey = '71KRX13XPZMGR3D1Q85W78G2DSZ4JPMAEX';
+        }
+        else if (network === 'testnet.fantom') {
+            this.url = 'https://api-testnet.ftmscan.com/api';
+            this.apikey = '71KRX13XPZMGR3D1Q85W78G2DSZ4JPMAEX';
+        }
+        else if (network === 'optimistic' || network === 'kovan-optimistic') {
+            this.url = `https://api-${network}.etherscan.io/api`;
+            this.apikey = 'FEXS1HXVA4Y2RNTMEA8V1UTK21S4JWHH9U';
+        }
+        else if (network === 'moonbeam') {
+            this.url = 'https://api-moonbeam.moonscan.io/api';
+            this.apikey = '5EUFXW6TDC16VERF3D9SCWRRU6AEMTBHNJ';
         }
         else {
             this.url = `https://api-${network}.etherscan.io/api`;

src/ts/parserEtherscan.ts
@@ -8,7 +8,7 @@ import { topologicalSortClasses } from './filterClasses'
 
 const debug = require('debug')('sol2uml')
 
-const networks = <const>[
+export const networks = <const>[
     'mainnet',
     'ropsten',
     'kovan',
@@ -16,8 +16,19 @@ const networks = <const>[
     'goerli',
     'sepolia',
     'polygon',
-    'bsc',
+    'testnet.polygon',
     'arbitrum',
+    'testnet.arbitrum',
+    'avalanche',
+    'testnet.avalanche',
+    'bsc',
+    'testnet.bsc',
+    'crono',
+    'fantom',
+    'testnet.fantom',
+    'moonbeam',
+    'optimistic',
+    'kovan-optimistic',
 ]
 type Network = typeof networks[number]
 
@@ -37,11 +48,41 @@ export class EtherscanParser {
         } else if (network === 'polygon') {
             this.url = 'https://api.polygonscan.com/api'
             this.apikey = 'AMHGNTV5A7XYGX2M781JB3RC1DZFVRWQEB'
-        } else if (network === 'bsc') {
-            this.url = 'https://api.bscscan.com/api'
-            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN'
+        } else if (network === 'testnet.polygon') {
+            this.url = 'https://api-testnet.polygonscan.com/api'
+            this.apikey = 'AMHGNTV5A7XYGX2M781JB3RC1DZFVRWQEB'
         } else if (network === 'arbitrum') {
             this.url = 'https://api.arbiscan.io/api'
+            this.apikey = 'ZGTK2TAGWMAB6IAC12BMK8YYPNCPIM8VDQ'
+        } else if (network === 'testnet.arbitrum') {
+            this.url = 'https://api-testnet.arbiscan.io/api'
+            this.apikey = 'ZGTK2TAGWMAB6IAC12BMK8YYPNCPIM8VDQ'
+        } else if (network === 'avalanche') {
+            this.url = 'https://api.snowtrace.io/api'
+            this.apikey = 'U5FAN98S5XNH5VI83TI4H35R9I4TDCKEJY'
+        } else if (network === 'testnet.avalanche') {
+            this.url = 'https://api-testnet.snowtrace.io/api'
+            this.apikey = 'U5FAN98S5XNH5VI83TI4H35R9I4TDCKEJY'
+        } else if (network === 'bsc') {
+            this.url = 'https://api.bscscan.com/api'
+        } else if (network === 'testnet.bsc') {
+            this.url = 'https://api-testnet.bscscan.com/api'
+        } else if (network === 'crono') {
+            this.url = 'https://api.cronoscan.com/api'
+            this.apikey = '76A3RG5WHTPMMR66E9SFI2EIDT6MP976W2'
+            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN'
+        } else if (network === 'fantom') {
+            this.url = 'https://api.ftmscan.com/api'
+            this.apikey = '71KRX13XPZMGR3D1Q85W78G2DSZ4JPMAEX'
+        } else if (network === 'testnet.fantom') {
+            this.url = 'https://api-testnet.ftmscan.com/api'
+            this.apikey = '71KRX13XPZMGR3D1Q85W78G2DSZ4JPMAEX'
+        } else if (network === 'optimistic' || network === 'kovan-optimistic') {
+            this.url = `https://api-${network}.etherscan.io/api`
+            this.apikey = 'FEXS1HXVA4Y2RNTMEA8V1UTK21S4JWHH9U'
+        } else if (network === 'moonbeam') {
+            this.url = 'https://api-moonbeam.moonscan.io/api'
+            this.apikey = '5EUFXW6TDC16VERF3D9SCWRRU6AEMTBHNJ'
         } else {
             this.url = `https://api-${network}.etherscan.io/api`
         }

lib/parserEtherscan.d.ts
@@ -1,6 +1,6 @@
 import { ASTNode } from '@solidity-parser/parser/dist/src/ast-types';
 import { UmlClass } from './umlClass';
-declare const networks: readonly ["mainnet", "ropsten", "kovan", "rinkeby", "goerli", "sepolia", "polygon", "bsc", "arbitrum"];
+export declare const networks: readonly ["mainnet", "ropsten", "kovan", "rinkeby", "goerli", "sepolia", "polygon", "testnet.polygon", "arbitrum", "testnet.arbitrum", "avalanche", "testnet.avalanche", "bsc", "testnet.bsc", "crono", "fantom", "testnet.fantom", "moonbeam", "optimistic", "kovan-optimistic"];
 declare type Network = typeof networks[number];
 export declare class EtherscanParser {
     protected apikey: string;

lib/sol2uml.js
@@ -10,8 +10,12 @@ const converterClasses2Storage_1 = require("./converterClasses2Storage");
 const converterStorage2Dot_1 = require("./converterStorage2Dot");
 const regEx_1 = require("./utils/regEx");
 const writerFiles_1 = require("./writerFiles");
+const path_1 = require("path");
 const program = new commander_1.Command();
-program.version(require('../package.json').version);
+const version = (0, path_1.basename)(__dirname) === 'lib'
+    ? require('../package.json').version // used when run from compile js in /lib
+    : require('../../package.json').version; // used when run from TypeScript source files under src/ts via ts-node
+program.version(version);
 const debugControl = require('debug');
 const debug = require('debug')('sol2uml');
 program
@@ -30,20 +34,10 @@ The Solidity code can be pulled from verified source code on Blockchain explorer
     .option('-o, --outputFileName <value>', 'output file name')
     .option('-i, --ignoreFilesOrFolders <filesOrFolders>', 'comma separated list of files or folders to ignore')
     .addOption(new commander_1.Option('-n, --network <network>', 'Ethereum network')
-    .choices([
-    'mainnet',
-    'polygon',
-    'bsc',
-    'arbitrum',
-    'ropsten',
-    'kovan',
-    'rinkeby',
-    'goerli',
-    'sepolia',
-])
+    .choices(parserEtherscan_1.networks)
     .default('mainnet')
     .env('ETH_NETWORK'))
-    .addOption(new commander_1.Option('-k, --apiKey <key>', 'Etherscan, Polygonscan, BscScan or Arbiscan API key').env('SCAN_API_KEY'))
+    .addOption(new commander_1.Option('-k, --apiKey <key>', 'Blockchain explorer API key. eg Etherscan, Arbiscan, BscScan, CronoScan, FTMScan, PolygonScan or SnowTrace API key').env('SCAN_API_KEY'))
     .option('-v, --verbose', 'run with debugging statements', false);
 program
     .command('class', { isDefault: true })
@@ -90,7 +84,8 @@ If an Ethereum address with a 0x prefix is passed, the verified source code from
         debug(`Finished generating UML`);
     }
     catch (err) {
-        console.error(`Failed to generate UML diagram\n${err.stack}`);
+        console.error(err);
+        process.exit(2);
     }
 });
 program
@@ -143,7 +138,8 @@ WARNING: sol2uml does not use the Solidity compiler so may differ with solc. A k
         await (0, writerFiles_1.writeOutputFiles)(dotString, fileFolderAddress, contractName || 'storageDiagram', combinedOptions.outputFormat, combinedOptions.outputFileName);
     }
     catch (err) {
-        console.error(`Failed to generate storage diagram.\n${err.stack}`);
+        console.error(err.stack);
+        process.exit(2);
     }
 });
 program
@@ -170,7 +166,8 @@ In order for the merged code to compile, the following is done:
         await (0, writerFiles_1.writeSolidity)(solidityCode, outputFilename);
     }
     catch (err) {
-        console.error(`Failed to flatten files.\n${err.stack}`);
+        console.error(err);
+        process.exit(2);
     }
 });
 program.on('option:verbose', () => {

src/ts/sol2uml.ts
@@ -2,7 +2,7 @@
 
 import { convertUmlClasses2Dot } from './converterClasses2Dot'
 import { parserUmlClasses } from './parserGeneral'
-import { EtherscanParser } from './parserEtherscan'
+import { EtherscanParser, networks } from './parserEtherscan'
 import { classesConnectedToBaseContracts } from './filterClasses'
 import { Command, Option } from 'commander'
 import {
@@ -12,8 +12,14 @@ import {
 import { convertStorages2Dot } from './converterStorage2Dot'
 import { isAddress } from './utils/regEx'
 import { writeOutputFiles, writeSolidity } from './writerFiles'
+import { basename } from 'path'
 const program = new Command()
-program.version(require('../package.json').version)
+
+const version =
+    basename(__dirname) === 'lib'
+        ? require('../package.json').version // used when run from compile js in /lib
+        : require('../../package.json').version // used when run from TypeScript source files under src/ts via ts-node
+program.version(version)
 
 const debugControl = require('debug')
 const debug = require('debug')('sol2uml')
@@ -47,24 +53,14 @@ The Solidity code can be pulled from verified source code on Blockchain explorer
     )
     .addOption(
         new Option('-n, --network <network>', 'Ethereum network')
-            .choices([
-                'mainnet',
-                'polygon',
-                'bsc',
-                'arbitrum',
-                'ropsten',
-                'kovan',
-                'rinkeby',
-                'goerli',
-                'sepolia',
-            ])
+            .choices(networks)
             .default('mainnet')
             .env('ETH_NETWORK')
     )
     .addOption(
         new Option(
             '-k, --apiKey <key>',
-            'Etherscan, Polygonscan, BscScan or Arbiscan API key'
+            'Blockchain explorer API key. eg Etherscan, Arbiscan, BscScan, CronoScan, FTMScan, PolygonScan or SnowTrace API key'
         ).env('SCAN_API_KEY')
     )
     .option('-v, --verbose', 'run with debugging statements', false)
@@ -165,7 +161,8 @@ If an Ethereum address with a 0x prefix is passed, the verified source code from
 
             debug(`Finished generating UML`)
         } catch (err) {
-            console.error(`Failed to generate UML diagram\n${err.stack}`)
+            console.error(err)
+            process.exit(2)
         }
     })
 
@@ -268,7 +265,8 @@ WARNING: sol2uml does not use the Solidity compiler so may differ with solc. A k
                 combinedOptions.outputFileName
             )
         } catch (err) {
-            console.error(`Failed to generate storage diagram.\n${err.stack}`)
+            console.error(err.stack)
+            process.exit(2)
         }
     })
 
@@ -311,7 +309,8 @@ In order for the merged code to compile, the following is done:
                 combinedOptions.outputFileName || contractName
             await writeSolidity(solidityCode, outputFilename)
         } catch (err) {
-            console.error(`Failed to flatten files.\n${err.stack}`)
+            console.error(err)
+            process.exit(2)
         }
     })
 

commit 1cac8b63e4d3278bcd9f888a1be65cbfbcc24035

Fixed bug in BSC and BSC testnet config introduced in commit https://github.com/naddison36/sol2uml/commit/f6fe794c8fb0d5d12fdf416ab1c084fcefd0d170

lib/parserEtherscan.js
@@ -69,14 +69,15 @@ class EtherscanParser {
         }
         else if (network === 'bsc') {
             this.url = 'https://api.bscscan.com/api';
+            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN';
         }
         else if (network === 'testnet.bsc') {
             this.url = 'https://api-testnet.bscscan.com/api';
+            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN';
         }
         else if (network === 'crono') {
             this.url = 'https://api.cronoscan.com/api';
             this.apikey = '76A3RG5WHTPMMR66E9SFI2EIDT6MP976W2';
-            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN';
         }
         else if (network === 'fantom') {
             this.url = 'https://api.ftmscan.com/api';

src/ts/parserEtherscan.ts
@@ -67,12 +67,13 @@ export class EtherscanParser {
             this.apikey = 'U5FAN98S5XNH5VI83TI4H35R9I4TDCKEJY'
         } else if (network === 'bsc') {
             this.url = 'https://api.bscscan.com/api'
+            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN'
         } else if (network === 'testnet.bsc') {
             this.url = 'https://api-testnet.bscscan.com/api'
+            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN'
         } else if (network === 'crono') {
             this.url = 'https://api.cronoscan.com/api'
             this.apikey = '76A3RG5WHTPMMR66E9SFI2EIDT6MP976W2'
-            this.apikey = 'APYH49FXVY9UA3KTDI6F4WP3KPIC86NITN'
         } else if (network === 'fantom') {
             this.url = 'https://api.ftmscan.com/api'
             this.apikey = '71KRX13XPZMGR3D1Q85W78G2DSZ4JPMAEX'